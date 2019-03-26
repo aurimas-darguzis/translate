f1c67c1b0c80a7599c00cdcff0b8817591c20317
Aurimas <aurimas@darguzis.com>
2019-03-26 19:35:12 +0000

    feat(): added flags with click handlers

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+https://musing-yonath-2cc073.netlify.com/
 
 ## Available Scripts
 
--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,37 @@
 import React from 'react';
 
 class App extends React.Component {
+  state = { language: 'english' };
+
+  onLanguageChange = language => {
+    this.setState({ language });
+  };
+
   render() {
-    return <div>Hello World!</div>;
+    return (
+      <div className='ui container'>
+        <div>
+          Select a language:
+          <i
+            className='flag gb'
+            onClick={() => this.onLanguageChange('english')}
+          />
+          <i
+            className='flag lt'
+            onClick={() => this.onLanguageChange('lithuanian')}
+          />
+          <i
+            className='flag ru'
+            onClick={() => this.onLanguageChange('russian')}
+          />
+          <i
+            className='flag es'
+            onClick={() => this.onLanguageChange('spanish')}
+          />
+        </div>
+        {this.state.language}
+      </div>
+    );
   }
 }
 
